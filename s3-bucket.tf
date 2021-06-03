b662c68706abc8fc40faf1fe5aac721383555da9
mesuttunca <mesuttunca@gmail.com>
2021-06-03 20:02:47 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,8 @@
 module "s3-bucket" {
   source  = "app.terraform.io/mesut-training/s3-bucket/aws"
   version = "2.2.0"
-  
-  bucket = "Gaurav"
+
+  bucket = "Gauravmesuttunca"
   acl    = "private"
 
 }
